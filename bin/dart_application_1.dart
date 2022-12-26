246479f84594143659abc2bddba951e62a9ceb9f
Bruna Guimarães <bruna.guimaraes@qyon.com>
2022-12-26 10:38:05 -0300

    types in dart

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,3 +1,15 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  int idade = 29;
+  print(idade);
+
+  double altura = 1.59;
+  print(altura);
+
+  bool compare = (idade == altura);
+  print(compare);
+
+  String nome = "Bruna";
+  String sobrenome = "Guimarães";
+  print('Eu sou: \n'
+      '$nome $sobrenome');
 }
